ccb02bd0207a9e8adf6d2407aa94d2c173ad1a6d
Oleksandr Sytnyk <olsy@telenor.dk>
2020-05-13 21:50:19 +0300

    Added controller for leaderboard.

---
+++ QuizApi/QuizApi/Models/User.cs
@@ -0,0 +1,19 @@
+﻿namespace QuizApi.Models
+{
+	public class User
+	{
+		public User(
+			string id, 
+			string name, 
+			int score)
+		{
+			Id = id;
+			Name = name;
+			Score = score;
+		}
+
+		public string Id { get; }
+		public string Name { get; }
+		public int Score { get; }
+	}
+}
---
+++ QuizApi/QuizApi/Models/UserData.cs
@@ -0,0 +1,8 @@
+﻿namespace QuizApi.Models
+{
+	public class UserData
+	{
+		public string Name { get; set; }
+		public int Score { get; set; }
+	}
+}
