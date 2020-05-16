6e293a8aa4080380b00edf739e801c07d8c61674
Oleksandr Sytnyk <olsy@telenor.dk>
2020-05-17 02:18:30 +0300

    Updated leader mock

--- QuizApi/QuizApi/Services/LeadersMock.cs
+++ QuizApi/QuizApi/Services/LeadersMock.cs
@@ -5,11 +5,6 @@ namespace QuizApi.Services
 {
 	public static class LeadersMock
 	{
-		public static readonly List<User> Users = new List<User>
-		{
-			new User("1", "David", 2),
-			new User("2", "Oleg", 0),
-			new User("3", "Rachel", 1)
-		};
+		public static readonly List<User> Users = new List<User>();
 	}
 }
